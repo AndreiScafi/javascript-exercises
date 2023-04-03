e7dd77c8035e5c85ef0fcc96c72b45406a3c6525
Andrei Scafi <andreiscafi@hotmail.com>
2023-04-03 08:08:46 -0300

    Primeiro código 'hello world!'

---
+++ script.js
@@ -0,0 +1,10 @@
+// Escrevendo o primeiro código. 
+
+console.log('Hello World!');// Com esse método a mensagem irá aparecer no console da página.
+
+alert('Hello World!');// Já esse médto irá mostra a mensagem em uma janela de alerta. 
+
+
+
+
+
