8152f2b90a6639ce56b60a7de132a9dce891830b
Andrei Scafi <andreiscafi@hotmail.com>
2023-04-03 08:21:22 -0300

    Conceito de algorítimo e suas implicações práticas.

--- script.js
+++ script.js
@@ -4,7 +4,36 @@ console.log('Hello World!');// Com esse método a mensagem irá aparecer no cons
 
 alert('Hello World!');// Já esse médto irá mostra a mensagem em uma janela de alerta. 
 
+/* Conceito de algorítimo: sequencia de ordens ou comandos que deverão ser executadas por um computador.  */
 
+/* Conceito de algorítimo elaborado pelo Chat GPT: 
+
+Um algoritmo é uma sequência finita de instruções ou regras bem definidas que descrevem um conjunto de operações a serem executadas em um determinado conjunto de dados para produzir um resultado desejado.
+
+Em outras palavras, um algoritmo é um conjunto de passos lógicos que, quando seguidos corretamente, levarão à solução de um problema específico. Os algoritmos são usados em muitas áreas, como ciência da computação, matemática, engenharia, física e outras.
+
+Os algoritmos são essenciais para a computação e a programação de computadores, pois fornecem uma maneira de expressar soluções computacionais de forma clara e sistemática. Algoritmos são usados em uma variedade de aplicações, como processamento de imagem, criptografia, inteligência artificial, simulações científicas, entre outras.
+
+Um exemplo simples de algoritmo é uma receita de culinária. A receita contém uma sequência de instruções bem definidas que descrevem as etapas necessárias para produzir um prato específico. Cada etapa é claramente definida e segue uma ordem específica para produzir o resultado final desejado. */
+
+/* IMPORTANTE: Os comandos devem ser claros, precisos e lógicos. Qualquer erro de lógica na programação irá comprometer a funcionalidade e eficiência do algorítimo. */
+
+/* IMPORTANTE: A ordem dos comandos influênica a execução do algorítimo. Exemplo, caso se pretenda que o algorítimo escreva uma carta e depois a envie, todos os passos do processo deverão ocorrer em ordem lógica e cornológica:
+
+- Pegar um papel em branco;
+- Pegar uma caneta;
+- Escrever a mensagem;
+- Colocar a carta no envelope;
+- Selar a carta;
+- Enviar a carta;
+
+Se a ordem dos comandos for alterada, haverá um erro na execução do algorítimo.*/
+
+/* Console do Google Chrome. O console é acessado através do botão 'inspecionar'.
+
+Monitorar a execução do código através do console é fundamental para detectar possíveis bugs e erros de execução.
+
+*/
 
 
 
